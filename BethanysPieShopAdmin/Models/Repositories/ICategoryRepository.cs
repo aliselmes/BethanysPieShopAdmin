2512379947ce7d71870e1e69c09038f0d218585a
Alasdair Selmes <aselmes@sullivanbranding.com>
2024-07-18 13:18:22 +0100

    adding details pages

---
+++ BethanysPieShopAdmin/Models/Repositories/ICategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace BethanysPieShopAdmin.Models.Repositories
+{
+    public interface ICategoryRepository
+    {
+        IEnumerable<Category> GetAllCategories();
+
+        Task<IEnumerable<Category>> GetAllCategoriesAsync();
+
+        Task<Category?> GetCategoryByIdAsync(int id);
+    }
+}
---
+++ BethanysPieShopAdmin/Models/Repositories/IOrderRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace BethanysPieShopAdmin.Models.Repositories
+{
+    public interface IOrderRepository
+    {
+        Task<Order?> GetOrderDetailsAsync(int? orderId);
+
+        Task<IEnumerable<Order>> GetAllOrdersWithDetailsAsync();
+    }
+}
