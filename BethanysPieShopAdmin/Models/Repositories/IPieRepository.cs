861a9aedb3ed7fb31aa96bef2d6ecd2d126a86e8
Alasdair Selmes <aselmes@sullivanbranding.com>
2024-07-24 14:57:21 +0100

    add ordering, filtering, sorting

--- BethanysPieShopAdmin/Models/Repositories/IPieRepository.cs
+++ BethanysPieShopAdmin/Models/Repositories/IPieRepository.cs
@@ -11,5 +11,13 @@
         Task<int> UpdatePieAsync(Pie pie);
 
         Task<int> DeletePieAsync(int id);
+
+        Task<int> GetAllPiesCountAsync();
+
+        Task<IEnumerable<Pie>> GetPiesPagedAsync(int? pageNumber, int pageSize);
+
+        Task<IEnumerable<Pie>> GetPiesSortedAndPagedAsync(string sortBy, int? pageNumber, int pageSize);
+
+        Task<IEnumerable<Pie>> SearchPies(string searchQuery, int? categoryId);
     }
 }
